81b54b22d2d4bb6ea53f08c6a5ef0579ac70e55f
MH2033 <mh.akbari@intuitiverobotics.com.au>
2021-11-26 23:24:14 +1100

    Initial tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <thread> 
+#include "worker_manager.hpp"
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+using namespace std;
+using namespace ir;
+
+int main() {
+	auto queue = WorkerManager::CreateWorkerCrew(10, "test");
+	queue->appendListener(1, [](WorkerManager::Event, void *){
+		cout << "Handler called" << endl;
+		auto var = new int[1000];
+		this_thread::sleep_for(std::chrono::seconds(1));
+		delete var;
+	});
+
+	for(int i = 0; i < 100; i++)
+		queue->enqueue(WorkerManager::Event{1, 1}, nullptr);
+
+	this_thread::sleep_for(std::chrono::seconds(10));
+
+
+	WorkerManager::KillWorkerCrew("test");
+
+	cout << "Worker Crew terminated" << endl;
+	this_thread::sleep_for(std::chrono::seconds(20));
+
+
 }
