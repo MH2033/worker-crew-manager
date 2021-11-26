0f982bef247e26e477424ef45c243bfc1cfd2b4b
MH2033 <mh.akbari@intuitiverobotics.com.au>
2021-11-26 18:35:24 +1100

    Add header file for function definitions

--- include/worker_manager.hpp
+++ include/worker_manager.hpp
@@ -4,14 +4,53 @@
 #include <map>
 #include <vector>
 #include <pthread.h>
+#include <chrono>
+#include "eventpp/utilities/orderedqueuelist.h"
+#include "eventpp/eventqueue.h"
 
 
 namespace ir {
+  
 class WorkerManager {
+private:
+
+  struct Event {
+    int e;
+    int priority;
+  };
+
+  struct Compare {
+    template <typename T>
+    bool operator() (const T & a, const T & b) const {
+      return std::get<0>(a.arguments).priority > std::get<0>(b.arguments).priority;
+      }
+  };
+
+  struct QueuePolicy{
+    template <typename Item>
+    using QueueList = eventpp::OrderedQueueList<Item, Compare >;
+
+    static int getEvent(const Event & event, void * const a) {
+      return event.e;
+    }
+  };
+
+  using PriorityEventQueue = eventpp::EventQueue<int, void(const Event &, void * const), QueuePolicy>;
+
+  struct WorkerCrew  {
+    std::vector<pthread_t > thread_ids;
+    PriorityEventQueue *worker_queue;
+  };
+  static std::map<std::string, WorkerCrew> workers;
+  static void *WorkerThread(void *);
+
 public:
-  static void CreateWorker(int num_threads, std::string worker_name);
-  static void KillWorker(std::string);
+  static PriorityEventQueue CreateWorkerCrew(int num_threads, std::string worker_name);
+  static bool KillWorkerCrew(std::string, std::chrono::duration<std::chrono::milliseconds>);
+
+  static PriorityEventQueue get_queue(std::string);
 };
+
 }
 
 #endif
