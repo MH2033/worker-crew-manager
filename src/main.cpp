65e52d8c1b4f2b8d19fe0cfee57ef8f659f91b8a
MH2033 <mh.akbari@hotmail.com>
2021-11-28 10:36:08 +0330

    Pass parameter to handler

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,19 @@ using namespace std;
 using namespace ir;
 
 int main() {
-	auto queue = WorkerManager::CreateWorkerCrew(10, "test");
-	queue->appendListener(1, [](WorkerManager::Event, void *){
-		cout << "Handler called" << endl;
+	auto queue = WorkerManager::CreateWorkerCrew(2, "test");
+	queue->appendListener(1, [](WorkerManager::Event, void *param){
+		auto num = (int *)param;
+		cout << "Received number: " << *num << endl;
+		cout << "Handler called in thread: " << this_thread::get_id() << endl;
 		auto var = new int[1000];
-		this_thread::sleep_for(std::chrono::seconds(1));
+		// this_thread::sleep_for(std::chrono::seconds(1));
 		delete var;
 	});
 
+	int a = 5;
 	for(int i = 0; i < 100; i++)
-		queue->enqueue(WorkerManager::Event{1, 1}, nullptr);
+		queue->enqueue(WorkerManager::Event{1, 1}, &a);
 
 	this_thread::sleep_for(std::chrono::seconds(10));
 
